1fd69bca8557b1cbb419baed085570e3b4e97f26
LAPTOP-LKCOQMQM\user <limeh-wm20@student.tarc.edu.my>
2023-11-10 14:11:28 +0800

    update user module

--- routes/web.php
+++ routes/web.php
@@ -18,12 +18,37 @@ use App\Http\Controllers\UserController;
 Route::get('/', function () {
     return view('user/index');
 });
+Route::get('product', function () {
+    return view('user/product');
+});
 Route::get('/auth/google', [UserController::class, 'redirectToGoogle']);
 Route::get('/auth/google/callback', [UserController::class, 'handleGoogleCallback']);
 Route::get('logout', function () {
     session()->flush();
     return redirect('/');
 });
+Route::get('/forget_password', function () {
+    return view('user/forgetPassword');
+});
+Route::post('/forget_password', [UserController::class, 'forgetPassword']);
+Route::post('/submitResetPasswordForm', [UserController::class, 'submitResetPasswordForm']);
+Route::get('user/reset_password/{token}/{email}', [UserController::class, 'verify_reset_password'])->name('reset_password');
 
 Route::post('/register', [UserController::class, 'register']);
 Route::post('/login', [UserController::class, 'login']);
+
+// user
+Route::prefix('user')->middleware(['auth'])->group(function(){
+    Route::get('/edit-profile', [UserController::class, 'edit_profile'])->name('profile');
+    Route::post('/submitEditProfileForm/{id}', [UserController::class, 'submitEditProfileForm'])->name('submitEditProfileForm');
+    Route::get('/sendOTP/{phoneNumber}', [UserController::class, 'sendOTP']);
+    Route::get('/validateOTP/{otp}', [UserController::class, 'validateOTP']);
+});
+
+
+// admin
+Route::prefix('admin')->middleware(['auth'])->group(function(){
+    Route::resource('customers', UserController::class);
+});
+
+
